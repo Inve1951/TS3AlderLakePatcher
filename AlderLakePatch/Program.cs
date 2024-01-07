b9ed5b308d2b6f9c79683791d4a10cd3ac79418a
square <square3880@gmail.com>
2024-01-07 19:48:52 +0100

    migrate to net6; drop GUI

--- AlderLakePatch/Program.cs
+++ AlderLakePatch/Program.cs
@@ -1,57 +1,44 @@
-﻿using System;
-using System.IO;
-using System.Windows.Forms;
+﻿namespace AlderLakePatch;
 
-namespace AlderLakePatcher
+public static class Program
 {
-    static class Program
+    public static int Main()
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        var args = Environment.GetCommandLineArgs();
+        if (args.Length != 2)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            var file = new OpenFileDialog();
-            MessageBox.Show("Locate the executable you wish to patch.");
-            file.Filter = "Executable files (*.exe)|*.exe|All files (*.*)|*.*";
-            var result = file.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                var intelPath = Path.Combine(Path.GetDirectoryName(file.FileName), "IntelFix.dll");
-                var backupName = Path.GetFileNameWithoutExtension(file.FileName) + ".bak";
-                var backupPath = Path.Combine(Path.GetDirectoryName(file.FileName), backupName);
-                var madeBackup = false;
-                try
-                {
-                    File.Copy(file.FileName, backupPath, true);
-                    madeBackup = true;
-                }
-                catch (Exception e)
-                {
-                    madeBackup = false;
-                    var proceedResult = MessageBox.Show("Couldn't create a backup. Proceed anyway?", "Warning", MessageBoxButtons.YesNo);
-                    if (proceedResult != DialogResult.Yes)
-                        return;
-                }
-                try
-                {
-                    var peFile = new PeNet.PeFile(file.FileName);
-                    peFile.AddImport("IntelFix.dll", "_DllMain@12");
-                    File.WriteAllBytes(file.FileName, peFile.RawFile.ToArray());
-                    File.Copy("IntelFix.dll", intelPath, true);
-                    if (madeBackup)
-                        MessageBox.Show("Executable patched successfully! Created a backup as " + backupName);
-                    else
-                        MessageBox.Show("Executable patched successfully.");
-                }
-                catch (Exception e)
-                {
-                    MessageBox.Show("Failed to patch executable!"+Environment.NewLine+"Error: "+Environment.NewLine+e.ToString());
-                }
-            }
+            Console.Error.WriteLine($"Usage: dotnet {args[0]} <exe path>");
+            return 1;
         }
+
+        var filePath   = args[1];
+        var intelPath  = Path.Combine(Path.GetDirectoryName(filePath) ?? ".", "IntelFix.dll");
+        var backupName = Path.GetFileNameWithoutExtension(filePath) + ".bak";
+        var backupPath = Path.Combine(Path.GetDirectoryName(filePath) ?? ".", backupName);
+        try
+        {
+            File.Copy(filePath, backupPath, true);
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"Failed to write backup to '{backupPath}': {ex}");
+            return 1;
+        }
+
+        try
+        {
+            var peFile = new PeNet.PeFile(filePath);
+            peFile.AddImport("IntelFix.dll", "_DllMain@12");
+            File.WriteAllBytes(filePath, peFile.RawFile.ToArray());
+            File.Copy("IntelFix.dll", intelPath, true);
+            Console.Out.WriteLine("Executable patched successfully! Created a backup as " + backupName);
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"Failed to patch executable: {ex}");
+            return 1;
+        }
+
+        return 0;
     }
 }
--- README.md
+++ README.md
@@ -4,9 +4,10 @@ Frontend for patching games to fix Alder Lake crashes on boot.
 Uses PeNet: https://github.com/secana/PeNet
 
 IntelFix.dll has to be placed in the program's working directory: https://github.com/LazyDuchess/IntelFix
+It is also vendored from https://github.com/LazyDuchess/AlderLakePatcher/releases/tag/1.0.1.
 
 Made for TS3 primarily, but could probably work for other games.
 
 ## Usage
 
-Simply open the program and locate the executable of the game you want to fix.
+Simply run the program with the executable of the game as the only command line argument.
